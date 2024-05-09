5143965ab94195e8cf815b281a9be31dc3a80e71
Mahamadou Simpara <mahamadousimpara@Simparas-MacBook-Air.local>
2024-05-09 13:25:48 +0000

    Handle error abd properly display it on the screen

--- frontend/src/pages/Error.jsx
+++ frontend/src/pages/Error.jsx
@@ -1,6 +1,29 @@
+import PageContent from "../components/PageContent";
+import { useRouteError } from 'react-router-dom'
+
 function ErrorPage () {
+
+    const error = useRouteError();
+
+    let title = 'An error occured!'
+    let message = 'Something went wrong !'
+
+    if(error.status === 500) {
+        title = 'Not found !'
+        message = 'Could not find resource on page !'
+
+    }
+
+    if(error.status === 400) {
+        title = 'An error occured!'
+
+    }
+    // console.log(error);
+
     return <div>
-        <h2>Error</h2>
+        <PageContent title={title}>
+            {message}
+        </PageContent>
     </div>
 }
 
